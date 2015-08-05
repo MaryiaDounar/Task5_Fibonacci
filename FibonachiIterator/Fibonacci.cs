24862c8848d11e2ec5d6b8525f954915647ca2eb
maryia_dounar <maryia_dounar@epam.com>
2015-08-05 16:05:36 +0300

    FirstCommit

---
+++ FibonachiIterator/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonachiIterator
+{
+   public class Fibonacci
+    {
+        public static IEnumerable<int> FibonacciSequence(int size)
+        {
+            int previous = 0;
+            int current = 1;
+            for(int i=1; i<size; i++)
+            {
+                yield return previous;
+                int next = previous + current;
+                previous = current;
+                current = next;
+            }
+        }
+    }
+}
---
+++ FibonachiIterator/Runner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonachiIterator
+{
+    class Runner
+    {
+        public static int size;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("\nPlease, enter a size");
+                try
+                {
+                    size = int.Parse(Console.ReadLine());
+                    if (size > 0)
+                    {
+                        foreach (int f in Fibonacci.FibonacciSequence(size))
+                        {
+                            Console.Write(f + "\t");
+                        }
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please, enter a positive number!");
+                }
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
